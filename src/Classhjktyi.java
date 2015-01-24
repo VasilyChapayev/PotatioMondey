bf0682df7c39887b6d8a0c3116d637ab3e111c5e
VasilyChapayev <swagkingyolosnapbacks@gmail.com>
2015-01-24 16:34:36 -0500

    More stuff

--- src/Classhjktyi.java
+++ src/Classhjktyi.java
@@ -1,4 +1,16 @@
 
-public class Classhjktyi {
+import java.util.Scanner;
 
+public class Classhjktyi {
+	
+	public static Scanner input = new Scanner(System.in);
+	public static String answer;
+	
+	public static void main(String[] args){
+		
+		System.out.println("Press p to smash potato.");
+		if("p".equals(answer)){
+			System.out.println("Smashed that potato.");
+		}
+	}
 }
